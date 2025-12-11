43d3e18e0b0a6cdc168530ef6a8fc858c05670bb
Darren Wong <58476988+darrenW04@users.noreply.github.com>
2025-12-10 20:26:17 -0500

    professor

---
+++ backend/tables/professor.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import VARCHAR
+
+from .database import Base
+
+
+class Professor(Base):
+    __tablename__ = "professor"
+
+    email = Column(VARCHAR(length=255), primary_key=True, nullable=False)
+    name = Column(VARCHAR(length=255), nullable=True)
+    title = Column(VARCHAR(length=255), nullable=True)
+    phone_number = Column(VARCHAR(length=255), nullable=True)
+    department = Column(VARCHAR(length=255), nullable=True)
+    portfolio_page = Column(VARCHAR(length=255), nullable=True)
+    profile_page = Column(VARCHAR(length=255), nullable=True)
+
+    # rcs = Column(VARCHAR(length=255), nullable = True)
